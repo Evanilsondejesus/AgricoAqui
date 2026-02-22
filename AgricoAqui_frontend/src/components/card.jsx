88d4a9653408490c450540d16c8ad2a28c4815eb
Evanilson Almeida <evaevanilson@gmail.com>
2026-02-22 16:17:49 -0300

    adaptando os tamanhos do cards para não deixa muito grandes ou em tamanho individual

--- AgricoAqui_frontend/src/components/card.jsx
+++ AgricoAqui_frontend/src/components/card.jsx
@@ -1,17 +1,4 @@
 
-  
-// export default function Card() {
-//   return (
-//     <div className="card" style={{ width: "18rem" }}>
-//       <img src="..." className="card-img-top" alt="..." />
-//       <div className="card-body">
-//         <p className="card-text">
-//           Some quick example text to build on the card title and make up the bulk of the card’s content.
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
 
 
 
@@ -19,45 +6,76 @@
 
 
 
-// Card.jsx
-export default function Card({ nome, preco, imagem, key, descricao, mostrarBotao, onClickBotao }) {
+
+// export default function Card({ nome, preco, imagem, key, descricao, mostrarBotao, onClickBotao }) {
+//   return (
+
+// <div key={key} className="col-6 col-md-3" >
+//     <div className="card" style={{ width: "18rem" }}>
+//       {imagem && <img src={imagem} className="card-img-top" alt={descricao} />}
+//       <div className="card-body">
+//         <h5 className="card-title">{nome}</h5>
+//         <p className="card-text">$ {preco}</p>
+
+//         {mostrarBotao && (
+//           <button className="btn btn-primary" onClick={onClickBotao} >
+//             Adicionar ao carrinho
+//           </button>
+//         )}
+//       </div>
+//     </div>
+
+
+
+// </div>
+
+
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default function Card({ imagem, nome, preco, mostrarBotao, onClickBotao }) {
   return (
+    <div className="col-md-4 col-lg-3 d-flex">
+      <div className="card h-100 w-100 shadow-sm">
 
-<div key={key} className="col-6 col-md-3" >
-    <div className="card" style={{ width: "18rem" }}>
-      {imagem && <img src={imagem} className="card-img-top" alt={descricao} />}
-      <div className="card-body">
-        <h5 className="card-title">{nome}</h5>
-        <p className="card-text">$ {preco}</p>
+        <div className="image-container">
+          <img src={imagem} className="card-img-top img-padronizada" alt={nome} />
+        </div>
 
-        {mostrarBotao && (
-          <button className="btn btn-primary" onClick={onClickBotao} >
-            Adicionar ao carrinho
-          </button>
-        )}
+        <div className="card-body d-flex flex-column">
+          <h5 className="card-title">{nome}</h5>
+          <p className="card-text fw-bold">R$ {preco}</p>
+
+          {mostrarBotao && (
+            <button
+              className="btn btn-success mt-auto"
+              onClick={onClickBotao}
+            >
+              Adicionar ao carrinho
+            </button>
+          )}
+        </div>
+
       </div>
     </div>
-
-
-
-
-
-
-
-
-</div>
-
-
-
-
-    
-
-
-
-
-
-
-
-
   );
 }
